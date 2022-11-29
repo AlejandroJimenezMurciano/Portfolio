1790d3ed3d550b6943aa5449b42849e031127add
AlejandroJimenezMurciano <alejimenezmurciano@gmail.com>
2022-11-29 18:48:36 +0100

    portfolio

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,6 +1,54 @@
 const toggleMenuElement = document.getElementById('toggle-menu');
-const MenuElement = document.getElementById('menu')
+const menuElement = document.getElementById('menu');
+const rightArrowElement = document.getElementById('arrow-icon-right');
+const leftArrowElement = document.getElementById('arrow-icon-left');
+const allSliderItems = document.querySelectorAll('.projects__descriptions')
+const rootStyles = document.documentElement.style
+
+let counter = 0;
+let translate = 0;
+
+const moveSlide = () => {
+    rootStyles.setProperty('--slider-translate', 100 * translate * -1 + '%')
+}
+
+const nextSlide = () => {
+
+    console.log('next')
+    console.log(counter)
+    console.log(translate)
+
+    if (counter >= allSliderItems.length - 2) {
+        counter = 0;
+        translate = 0
+    }
+    else {
+        counter = counter + 1;
+        translate = translate + 1
+    }
+
+    moveSlide()
+}
+
+const prevSlide = () => {
+    if (counter <= 0) {
+        counter = allSliderItems.length - 2
+        translate = allSliderItems.length - 2
+    }
+    else {
+        counter = counter - 1;
+        translate = translate - 1;
+    }
+
+    moveSlide()
+}
+
+
 
 toggleMenuElement.addEventListener('click', () => {
-    MenuElement.classList.toggle('menu--show');
+    menuElement.classList.toggle('menu--show');
 });
+
+rightArrowElement.addEventListener('click', nextSlide)
+
+leftArrowElement.addEventListener('click', prevSlide)
